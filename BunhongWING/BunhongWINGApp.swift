dbb7f86a45d4595727710682864602172806d256
Saingkoing SEA <sea.saingkoing@gmail.com>
2022-05-29 16:18:57 +0700

    Initial Commit

---
+++ BunhongWING/BunhongWINGApp.swift
@@ -0,0 +1,17 @@
+//
+//  BunhongWINGApp.swift
+//  BunhongWING
+//
+//  Created by KoingDev on 29/5/22.
+//
+
+import SwiftUI
+
+@main
+struct BunhongWINGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
